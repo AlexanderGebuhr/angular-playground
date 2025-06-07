5aa6df8530ef8e61cffbf897b8a64a24b53875bc
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2025-06-07 14:04:04 +0200

    Improvements

--- apps/angular-playground/src/app/mfe/mfe-routes.ts
+++ apps/angular-playground/src/app/mfe/mfe-routes.ts
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 import { Routes } from '@angular/router';
-import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const mfeRoutes: Routes = [
   {
@@ -10,8 +7,6 @@ export const mfeRoutes: Routes = [
   },
   {
     path: 'mfe-app',
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    loadComponent: () => loadRemoteModule('angular-playground-mfe', './Component').then(m => m.AppComponent),
+    loadChildren: () => import('./mfe-app/mfe-app').then(m => m.mfeAppRoutes),
   },
 ];
